0a3ecd70e235a341fc0c8b8721797d534e381320
Megha <megha893.arora@gmail.com>
2016-07-19 17:55:27 +0530

    Updated views after API fixes

--- app/scripts/controllers/dashboard.js
+++ app/scripts/controllers/dashboard.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// Fix converter and geo-tag balancer, balancer
-
 function dashboardCtrl($scope, $interval, eosService, COLORS) {
 
   $scope.switchQuota = function (value) {
@@ -9,11 +7,9 @@ function dashboardCtrl($scope, $interval, eosService, COLORS) {
   };
   $scope.switchBalancer = function (value) {
     eosService.setSpaceConfig('default', 'space.balancer',$scope.checkState(value));
-    console.log('EOS', value);
   };
-  //TODO: geotagbalancer doesn't take this command http://eosservicetest2:8000/proc/admin/?eos.rgid=0&eos.ruid=0&mgm.cmd=space&mgm.subcmd=config&callback=JSON_CALLBACK&mgm.space.name=default&mgm.space.key=space.geotagbalancer&mgm.space.value=on
-  $scope.switchGeoTagBalancer = function (value) {
-    eosService.setSpaceConfig('default', 'space.geotagbalancer',$scope.checkState(value));
+  $scope.switchGeoBalancer = function (value) {
+    eosService.setSpaceConfig('default', 'space.geobalancer',$scope.checkState(value));
   };
   $scope.switchGroupBalancer = function (value) {
     eosService.setSpaceConfig('default', 'space.groupbalancer',$scope.checkState(value));
@@ -59,10 +55,12 @@ function dashboardCtrl($scope, $interval, eosService, COLORS) {
 
   eosService.getNodes().success(function (response) {
       $scope.nodes = response[0].node.ls;
+      console.log($scope.nodes);
     });
 
   eosService.getSpaces().success(function (response) {
       $scope.spaces = response[0].space.ls;
+      console.log($scope.spaces);
     });
 
   eosService.getVersion().success(function (response) {
@@ -76,12 +74,9 @@ function dashboardCtrl($scope, $interval, eosService, COLORS) {
 
   eosService.getSpaceStatus().success(function (response) {
       $scope.spaceStatus = response[0];
-      // TODO: Balncer status always off in API response even if it is on in CLI
       $scope.balancer =  $scope.checkValue(response[0].space.status[0].balancer.status);
-      console.log($scope.balancer);
-      // TODO: Get on/off status for converterin json
-      $scope.converter =  $scope.checkValue(response[0].space.status[0].balancer.status);
-      $scope.geoTagBalancer =  $scope.checkValue(response[0].space.status[0].geotagbalancer.status);
+      $scope.converter =  $scope.checkValue(response[0].space.status[0].converter.status);
+      $scope.geoBalancer =  $scope.checkValue(response[0].space.status[0].geobalancer.status);
       $scope.groupBalancer =  $scope.checkValue(response[0].space.status[0].groupbalancer.status);
       $scope.quota =  $scope.checkValue(response[0].space.status[0].quota);
     });
